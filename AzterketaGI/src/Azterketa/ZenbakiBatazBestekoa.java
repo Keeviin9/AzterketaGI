f50f1c9cc6744492f596d941f5492a93c06daf46
Keeviin9 <146726248+Keeviin9@users.noreply.github.com>
2023-11-20 13:38:32 +0100

    Add files via upload

---
+++ AzterketaGI/src/Azterketa/ZenbakiBatazBestekoa.java
@@ -0,0 +1,42 @@
+package Azterketa;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ZenbakiBatazBestekoa {
+
+	    public static void main(String[] args) {
+	        ArrayList<Double> zenbakiak = new ArrayList<>();
+	        Scanner scanner = new Scanner(System.in);
+
+	        while (true) {
+	            try {
+	                System.out.print("Sartu zenbakiak gero guztien bataz bestekoa kalkulatzeko (0 sartu bukatzeko): ");
+	                double zenbaki = scanner.nextDouble();
+	                
+	                if (zenbaki == 0) {
+	                    break;
+	                }
+	                
+	                zenbakiak.add(zenbaki);
+	            } catch (Exception e) {
+	                System.out.println("Errorea! Zenbaki bat sartu behar duzu.");
+	                scanner.nextLine(); 
+	            }
+	        }
+
+	        if (zenbakiak.size() > 0) {
+	            double batazbeste = 0;
+	            for (double zenbaki : zenbakiak) {
+	                batazbeste += zenbaki;
+	            }
+	            batazbeste /= zenbakiak.size();
+	            
+	            System.out.println("Zenbaki guztien bataz bestekoa: " + batazbeste + " da.");
+	        } else {
+	            System.out.println("Ez zenbaki sartu.");
+	        }
+
+	        scanner.close();
+	    }
+	}
